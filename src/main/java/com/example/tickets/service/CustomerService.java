8150957c32ee55adf19081ac9205b2ae1ab43091
Maryna <nikishina.maryna@gmail.com>
2021-12-03 19:12:08 +0200

    commit 12

--- src/main/java/com/example/tickets/service/CustomerService.java
+++ src/main/java/com/example/tickets/service/CustomerService.java
@@ -7,6 +7,7 @@ import com.example.tickets.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,4 +17,6 @@ public class CustomerService {
     public List<Customer> getCustomer() {
         return customerRepository.findAll();
     }
+
+
 }
