712f63344efe6b7f7a340897eb749abba907a826
Maryna <nikishina.maryna@gmail.com>
2021-12-03 16:15:27 +0200

    commit2

--- src/main/java/com/example/tickets/controller/CustomerController.java
+++ src/main/java/com/example/tickets/controller/CustomerController.java
@@ -9,22 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Validated
 @RestController
-//@RequestMapping("/customer")
+@RequestMapping("/customer")
 public class CustomerController {
     @Autowired
     private CustomerService service;
-    @GetMapping("/allCustomers")
+    @GetMapping()
+  //  @GetMapping("/allCustomers")
     public List<Customer> findAllCustomers() {
         return service.getCustomer ();
     }
-
-
-
-
 }
