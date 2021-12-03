05bae145818274822d9192f8afb37cf53ae8f2f1
Maryna <nikishina.maryna@gmail.com>
2021-12-03 16:21:28 +0200

    commit 5

--- src/main/java/com/example/tickets/repository/CustomerRepository.java
+++ src/main/java/com/example/tickets/repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.example.tickets.repository;
 import com.example.tickets.entity.Customer;
 import com.example.tickets.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository  extends JpaRepository<Customer,Long> {
 }
