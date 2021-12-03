ded4327af0f43770a4862620ce6d668c035cafee
Maryna <nikishina.maryna@gmail.com>
2021-12-03 16:23:04 +0200

    commit 6

--- src/main/java/com/example/tickets/repository/EventRepository.java
+++ src/main/java/com/example/tickets/repository/EventRepository.java
@@ -2,8 +2,16 @@ package com.example.tickets.repository;
 
 import com.example.tickets.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface EventRepository extends JpaRepository<Event,Long> {
 
     Event findByTitle(String title);
+
+    @Query("select e from Event e join e.customers c where c.lastName = :lastName")
+    List<Event> findByLastName(@Param("lastName") String lastName);
 }
