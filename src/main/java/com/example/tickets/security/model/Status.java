b8433bc942e169c81d084e56eb14c894bab7743f
Maryna <nikishina.maryna@gmail.com>
2022-01-02 16:28:27 +0200

    Security

---
+++ src/main/java/com/example/tickets/security/model/Status.java
@@ -0,0 +1,5 @@
+package com.example.tickets.security.model;
+
+public enum Status {
+    ACTIVE, NOT_ACTIVE, DELETED
+}
---
+++ src/main/java/com/example/tickets/security/repositorySecurity/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.tickets.security.repositorySecurity;
+
+import com.example.tickets.security.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
